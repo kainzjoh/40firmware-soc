2d55f850be1618ab240a7682661ca6f00746bb8d
kainzjoh <kainzjoh@a1.net>
2018-08-15 09:21:29 +0200

    trying pep8

--- main.py
+++ main.py
@@ -8,18 +8,25 @@ import sys
 import argparse
 from easysnmp import Session
 
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-host","--hostname", help="Hostname", default="localhost")
-    parser.add_argument("-v","--version", type=int, choices=[1, 2], help="Version 1 or 2", default=2)
-    parser.add_argument("-p","--port", type=int, help="Specify port to use", default=161)
-    parser.add_argument("-c","--community", help="Specify Community to use", default="public")
+    parser.add_argument("-host", "--hostname", help="Hostname",
+                        default="localhost")
+    parser.add_argument("-v", "--version", type=int, choices=[1, 2],
+                        help="Version 1 or 2", default=2)
+    parser.add_argument("-p", "--port", type=int, help="Specify port to use",
+                        default=161)
+    parser.add_argument("-c", "--community", help="Specify Community to use",
+                        default="public")
     args = parser.parse_args()
     try:
-        ss = Session(hostname=args.hostname, community=args.community, version=args.version)
-        firmwareLong = ss.get('1.3.6.1.4.1.12356.101.4.1.1.0').value
-        firmware = firmwareLong[1:7]
-        firmware = firmware if firmware[5] != "," else firmware[0:5] # chop trailing "," if minor Version <9
+        ss = Session(hostname=args.hostname, community=args.community,
+                     version=args.version)
+        firmware_full = ss.get('1.3.6.1.4.1.12356.101.4.1.1.0').value
+        firmware = firmware_full[1:7]
+        # chop trailing "," if minor Version <9
+        firmware = firmware if firmware[5] != "," else firmware[0:5]
     except Exception as e:
         print("ERROR: " + e)
         sys.exit(3)
@@ -30,9 +37,9 @@ def main():
     except requests.exceptions.RequestException as e:
         print("ERROR: " + e)
         sys.exit(3)
-        
+
     r = json.loads(response.text)
-    cvelist = []  
+    cvelist = []
     for cve in r:
         try:
             for vulnerableconf in cve["vulnerable_configuration"]:
@@ -49,11 +56,11 @@ def main():
             print("Key in JSON Response not found!")
             sys.exit(3)
     if len(cvelist) != 0:
-        print("CRITICAL: Vulnerabilities found for " + firmwareLong, end=" ")
+        print("CRITICAL: Vulnerabilities found for " + firmware_full, end=" ")
         print(cvelist)
         sys.exit(2)
     else:
-        print("OK: No vulnerability found for " + firmwareLong)
+        print("OK: No vulnerability found for " + firmware_full)
         sys.exit(0)
 
 if __name__ == '__main__':
